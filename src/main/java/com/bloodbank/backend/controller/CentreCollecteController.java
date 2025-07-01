5aee0eca2592e8a94bf335167fc5b89439fd903b
fatimaezzahraadyani <fatimaezzahraadyani@gmail.com>
2025-07-01 17:09:14 +0200

    last update

--- src/main/java/com/bloodbank/backend/controller/CentreCollecteController.java
+++ src/main/java/com/bloodbank/backend/controller/CentreCollecteController.java
@@ -4,7 +4,6 @@ import com.bloodbank.backend.model.CentreCollecte;
 import com.bloodbank.backend.services.CentreCollecteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
