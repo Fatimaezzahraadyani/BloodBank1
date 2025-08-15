cb028ae607f7081216f7b36493e3fa74d83e2334
fatimaezzahraadyani <fatimaezzahraadyani@gmail.com>
2025-08-15 12:52:40 +0200

    ajouter la fonctionnalité de trouver un RV by donor

--- src/main/java/com/bloodbank/backend/controller/RendezVousController.java
+++ src/main/java/com/bloodbank/backend/controller/RendezVousController.java
@@ -1,6 +1,7 @@
 package com.bloodbank.backend.controller;
 
 
+import com.bloodbank.backend.model.Donneur;
 import com.bloodbank.backend.model.RendezVous;
 import com.bloodbank.backend.repository.CentreCollecteRepository;
 import com.bloodbank.backend.repository.DonorRepository;
@@ -8,6 +9,8 @@ import com.bloodbank.backend.repository.RendezVousRepository;
 import com.bloodbank.backend.services.RendezVousService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/rendezVous")
@@ -26,6 +29,12 @@ public class RendezVousController {
     @PostMapping
     public ResponseEntity<RendezVous> creat(@RequestBody RendezVous rendezVous){
         return ResponseEntity.ok(rendezVousService.create(rendezVous));
+    }
+
+    @GetMapping("/donneur/{donneurId}")
+    public ResponseEntity<List<RendezVous>> getByDonneur(@PathVariable Long donneurId){
+        Donneur donneur = donorRepository.findById(donneurId).orElseThrow();
+        return ResponseEntity.ok(rendezVousService.getByDonneur(donneur));
     }
 
 
