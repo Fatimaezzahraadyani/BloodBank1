55b1e9a18920d56708b769505f2b31c7d0dbf07f
fatimaezzahraadyani <fatimaezzahraadyani@gmail.com>
2025-08-31 18:28:58 +0200

    integrer sonar et fixer un erreur de securite

--- src/main/java/com/bloodbank/backend/security/AdminInitializer.java
+++ src/main/java/com/bloodbank/backend/security/AdminInitializer.java
@@ -3,8 +3,8 @@ package com.bloodbank.backend.security;
 
 import com.bloodbank.backend.model.Admin;
 import com.bloodbank.backend.model.Role;
-import com.bloodbank.backend.model.User;
 import com.bloodbank.backend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,19 +12,25 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class AdminInitializer {
+
     @Bean
-    CommandLineRunner createAdmin(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+    CommandLineRunner createAdmin(
+            UserRepository userRepository,
+            PasswordEncoder passwordEncoder,
+            @Value("${admin.email}") String adminEmail,     // ⚠️ INJECTION DIRECTE dans le paramètre
+            @Value("${admin.password}") String adminPassword // ⚠️ INJECTION DIRECTE dans le paramètre
+    ) {
         return args -> {
-            if (userRepository.findByEmail("Tima1234@gmail.com").isEmpty()) {
+            if (userRepository.findByEmail(adminEmail).isEmpty()) {
                 Admin admin = new Admin();
                 admin.setFirstName("Admin");
                 admin.setLastName("Dyn");
-                admin.setEmail("Tima1234@gmail.com");
-                admin.setPassword(passwordEncoder.encode("admin123"));
+                admin.setEmail(adminEmail);
+                admin.setPassword(passwordEncoder.encode(adminPassword));
                 admin.setRole(Role.ADMIN);
 
                 userRepository.save(admin);
-                System.out.println("✅ Admin user created: Tima1234@gmail.com / admin123");
+                System.out.println("✅ Admin user created:" + adminEmail);
             } else {
                 System.out.println("ℹ️ Admin user already exists");
             }
