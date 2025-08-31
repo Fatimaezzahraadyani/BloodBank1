b647ce6671d0aef28018af0e101cb3da25e96e4a
fatimaezzahraadyani <fatimaezzahraadyani@gmail.com>
2025-08-31 02:19:57 +0200

    ajouter docker-compose yml et lance containers

--- src/test/java/com/bloodbank/backend/Service/CentreServiceTest.java
+++ src/test/java/com/bloodbank/backend/Service/CentreServiceTest.java
@@ -4,10 +4,13 @@ package com.bloodbank.backend.Service;
 import com.bloodbank.backend.model.CentreCollecte;
 import com.bloodbank.backend.repository.CentreCollecteRepository;
 import com.bloodbank.backend.services.CentreCollecteService;
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
 
 import java.util.List;
 import java.util.Optional;
@@ -15,6 +18,7 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+
 public class CentreServiceTest {
 
     private CentreCollecteService centreCollecteService;
@@ -32,7 +36,7 @@ public class CentreServiceTest {
         CentreCollecte centreCollecte = new CentreCollecte();
         centreCollecte.setName("Centre moulay rchid");
         centreCollecte.setAdresse("bvd abcd");
-        centreCollecte.setVille("ogiugzuief");
+        centreCollecte.setVille("ooo");
 
         CentreCollecte saved = centreCollecteService.create(centreCollecte);
 
@@ -44,9 +48,9 @@ public class CentreServiceTest {
     @Test
     void testGetAllCentersCollecte(){
         CentreCollecte centre = new CentreCollecte();
-        centre.setName("center de dons");
-        centre.setAdresse("Bvd Mohammed 6");
-        centre.setVille("Beni Mellal");
+        centre.setName("center hjvjhf");
+        centre.setAdresse("Bvd Mohammed 5");
+        centre.setVille("new city");
 
         centreCollecteRepository.save(centre);
 
@@ -58,9 +62,9 @@ public class CentreServiceTest {
     @Test
     void testGetCentreById() {
         CentreCollecte centre = new CentreCollecte();
-        centre.setName("Centre Hassan II");
-        centre.setAdresse("Avenue Royale");
-        centre.setVille("Casablanca");
+        centre.setName("Centre Hassan 1");
+        centre.setAdresse("Avenuè Royal");
+        centre.setVille("abcde");
 
         CentreCollecte saved = centreCollecteRepository.save(centre);
 
@@ -68,7 +72,7 @@ public class CentreServiceTest {
                 .getCentreCollecteById(saved.getId())
                 .orElseThrow(() -> new AssertionError("Centre introuvable"));
 
-        assertEquals("Centre Hassan II", found.getName());
+        assertEquals("Centre Hassan 1", found.getName());
     }
 
 
@@ -76,15 +80,15 @@ public class CentreServiceTest {
     void testUpdateCentreCollecte() {
         //Créer un centre initial
         CentreCollecte centre = new CentreCollecte();
-        centre.setName("Centre Hassan II");
-        centre.setAdresse("Avenue Royale");
-        centre.setVille("is");
+        centre.setName("Centre Hassan P");
+        centre.setAdresse("Avenu Royale");
+        centre.setVille("mmm");
 
         CentreCollecte saved = centreCollecteRepository.save(centre);
 
         //Modifier les champs
         saved.setName("Centre Mohammed V");
-        saved.setAdresse("Boulevard Hassan II");
+        saved.setAdresse("Boulevard Hassan P");
 
         //Appeler la méthode update
         CentreCollecte updated = centreCollecteService.update(saved, saved.getId());
@@ -93,17 +97,17 @@ public class CentreServiceTest {
         assertNotNull(updated);
         assertEquals(saved.getId(), updated.getId()); //  le même ID
         assertEquals("Centre Mohammed V", updated.getName()); // Nouveau nom
-        assertEquals("Boulevard Hassan II", updated.getAdresse()); // Nouvelle adresse
-        assertEquals("is", updated.getVille()); // Ville n'a pas changé
+        assertEquals("Boulevard Hassan P", updated.getAdresse()); // Nouvelle adresse
+        assertEquals("mmm", updated.getVille()); // Ville n'a pas changé
     }
 
     @Test
     void testDeleteCentreCollecte(){
         //Créer un centre initial
         CentreCollecte centre = new CentreCollecte();
-        centre.setName("Centre Hassan II");
+        centre.setName("Centre sang");
         centre.setAdresse("Avenue Royale");
-        centre.setVille("aa");
+        centre.setVille("hhhh");
 
         CentreCollecte saved = centreCollecteRepository.save(centre);
 
