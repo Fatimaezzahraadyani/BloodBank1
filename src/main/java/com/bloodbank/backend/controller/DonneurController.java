b13728338097bfbdbb771bd510b3e70b9dc4c600
fatimaezzahraadyani <fatimaezzahraadyani@gmail.com>
2025-09-07 22:01:18 +0200

    compelete le profil d'un donor

--- src/main/java/com/bloodbank/backend/controller/DonneurController.java
+++ src/main/java/com/bloodbank/backend/controller/DonneurController.java
@@ -1,15 +1,16 @@
 package com.bloodbank.backend.controller;
 
 
+import com.bloodbank.backend.dto.DonorProfileUpdateDto;
+import com.bloodbank.backend.model.Donneur;
 import com.bloodbank.backend.model.User;
 import com.bloodbank.backend.repository.UserRepository;
+import com.bloodbank.backend.services.DonorService;
 import com.bloodbank.backend.services.UserServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
@@ -19,6 +20,8 @@ public class DonneurController {
 
     @Autowired
     private final UserRepository userRepository;
+    @Autowired
+    private DonorService donorService;
 
     public DonneurController(UserRepository userRepository) {
         this.userRepository = userRepository;
@@ -30,6 +33,19 @@ public class DonneurController {
         User user = userRepository.findUserByEmail(auth.getName());
         return ResponseEntity.ok(user);
     }
+
+    @PutMapping("/{donorId}/profile")
+    public ResponseEntity<Donneur> updateDonorProfile(@PathVariable Long donorId, @RequestBody DonorProfileUpdateDto updateDto){
+        Donneur updatedDonor = donorService.updateDonorProfile(donorId, updateDto);
+        return ResponseEntity.ok(updatedDonor);
+    }
+
+    @GetMapping("/{donorId}/profile")
+    public ResponseEntity<Donneur> getDonorProfile(@PathVariable Long donorId) {
+        Donneur donor = donorService.getDonorById(donorId);
+        return ResponseEntity.ok(donor);
+    }
+
 
 
 
--- src/main/java/com/bloodbank/backend/dto/DonorProfileUpdateDto.java
+++ src/main/java/com/bloodbank/backend/dto/DonorProfileUpdateDto.java
@@ -2,15 +2,87 @@ package com.bloodbank.backend.dto;
 
 import java.time.LocalDate;
 
-public record DonorProfileUpdateDto(
-         String bloodType,
-         LocalDate birthdate,
-         Double weight,
-         String maladieHistory,
-         String phone,
-         String address,
-         String maladiesChroniques,
-         Boolean priseDeMedicaments,
-         Boolean chirurgiesRecentes
-) {
+public class DonorProfileUpdateDto {
+    private String bloodType;
+    private LocalDate birthdate;
+    private Double weight;
+    private String maladieHistory;
+    private String phone;
+    private String address;
+    private String maladiesChroniques;
+    private Boolean priseDeMedicaments;
+    private Boolean chirurgiesRecentes;
+
+
+    public String getBloodType() {
+        return bloodType;
+    }
+
+    public void setBloodType(String bloodType) {
+        this.bloodType = bloodType;
+    }
+
+    public LocalDate getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(LocalDate birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public Double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Double weight) {
+        this.weight = weight;
+    }
+
+    public String getMaladieHistory() {
+        return maladieHistory;
+    }
+
+    public void setMaladieHistory(String maladieHistory) {
+        this.maladieHistory = maladieHistory;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getMaladiesChroniques() {
+        return maladiesChroniques;
+    }
+
+    public void setMaladiesChroniques(String maladiesChroniques) {
+        this.maladiesChroniques = maladiesChroniques;
+    }
+
+    public Boolean getPriseDeMedicaments() {
+        return priseDeMedicaments;
+    }
+
+    public void setPriseDeMedicaments(Boolean priseDeMedicaments) {
+        this.priseDeMedicaments = priseDeMedicaments;
+    }
+
+    public Boolean getChirurgiesRecentes() {
+        return chirurgiesRecentes;
+    }
+
+    public void setChirurgiesRecentes(Boolean chirurgiesRecentes) {
+        this.chirurgiesRecentes = chirurgiesRecentes;
+    }
 }
